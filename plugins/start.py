eb83ca49181c082a74974dccfa78cd4975cde4d6
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-12-12 10:54:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -237,8 +237,7 @@ async def delete_files(messages, client, k):
 
 
 
-# Jishu Developer 
+# Illegal Developer 
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @Illegal_Developer
+# Developer @Ishana_Dev_Bot
