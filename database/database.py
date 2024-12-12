aac25e3bf9c0ef3420ceb31b35089118153ce323
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-12-12 10:53:28 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -35,8 +35,7 @@ async def del_user(user_id: int):
 
 
 
-# Jishu Developer 
+# Illegal Developer 
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @Illegal_Developer
+# Developer @Ishana_Dev_Bot
