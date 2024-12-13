14847f8b70f012e08377e368336fad142066a9fb
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-12-13 10:57:57 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -156,8 +156,7 @@ subscribed = filters.create(is_subscribed)
 
 
 
-# Jishu Developer 
+# Illegal Developer Developer 
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @Illegal_Developer
+# Developer @Ishana_Dev_Bot
