cdded91bcd334f339ca1a5b9d417f2922ebf0aa6
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2024-12-13 11:01:02 +0530

    Update user_id.py

--- plugins/user_id.py
+++ plugins/user_id.py
@@ -19,10 +19,7 @@ async def showid(client, message):
 
 
 
-
-
-# Jishu Developer 
+# Illegal Developer Developer 
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @Illegal_Developer
+# Developer @Ishana_Dev_Bot
